1a9e601ecf2a597dcb70d655317177afd74c3f53
lehcode <53556648+lehcode@users.noreply.github.com>
2024-11-22 19:54:13 +0200

    chore(packages): Update project dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,33 @@
-/** @return {import('tailwindcss').Config} */
-module.exports = {
-  important: true,
-  corePlugins: {
-    container: false,
+/** @type {import('tailwindcss').Config} */
+import aspectRatio from "@tailwindcss/aspect-ratio";
+import typography from "@tailwindcss/typography";
+import colors from "tailwindcss/colors";
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+export default {
+  content: ["./index.html", "./src/**/*.{ts}"],
+  darkMode: "selector",
+  plugins: [typography, aspectRatio],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    colors: {
+      gray: colors.gray,
+      blue: colors.sky,
+      red: colors.rose,
+      pink: colors.fuchsia,
+      purple: "#7e5bef",
+      orange: "#ff7849",
+      green: "#13ce66",
+      yellow: "#ffc82c",
+      "gray-dark": "#273444",
+      "gray-light": "#d3dce6",
+    },
   },
-  theme: { 
-    extend: {},
-  },
-  plugins: [
-    require('@tailwindcss/typography')
-  ],
-}
+};
