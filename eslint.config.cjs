ab0a44bf65fbc2890675e67817e932c4a0489774
lehcode <53556648+lehcode@users.noreply.github.com>
2024-11-24 17:40:34 +0200

    fix(config): Update project config

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,57 +1,21 @@
-const jsConfigs = require('@eslint/js')
+// @ts-check
+const eslint = require('@eslint/js')
 const tseslint = require('typescript-eslint')
-const eslintPlugin = require('@nx/eslint-plugin')
-const tsParser = require('@typescript-eslint/parser')
-const { node, jest, vitest } = require('globals')
-const jsoncParser = require('jsonc-eslint-parser')
 const angular = require('angular-eslint')
 const pluginImport = require('eslint-plugin-import')
 const pluginSimpleImportSort = require('eslint-plugin-simple-import-sort')
 const pluginUnusedImports = require('eslint-plugin-unused-imports')
 
-module.exports = [
-  jsConfigs.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...tseslint.configs.stylistic,
-  ...eslintPlugin.configs['flat/base'],
-  ...eslintPlugin.configs['flat/typescript'],
-  ...eslintPlugin.configs['flat/javascript'],
-  ...angular.configs.tsRecommended,
+module.exports = tseslint.config(
   {
     ignores: ['node_modules', '**/*.js', '.angular'],
-  },
-  {
-    languageOptions: {
-      parser: tsParser,
-      globals: {
-        ...node,
-        ...jest,
-        ...vitest,
-      },
-    },
-    rules: {
-      semi: ['error', 'always'], // Require semicolons
-      'no-unused-vars': 'warn', // Warn on unused variables
-      'no-undef': 'warn', // Warn on undefined variables
-      'no-debugger': 'warn', // Warn on debugger statements
-    },
-  },
-  {
-    files: ['*.json'],
-    languageOptions: {
-      parser: jsoncParser,
-    },
-    rules: {},
-  },
-  {
-    files: ['**/*.cjs', '**/*.js'],
-    rules: {
-      '@typescript-eslint/no-require-imports': 'off',
-      'semi': 'off'
-    },
-  },
-  {
     files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+    ],
     processor: angular.processInlineTemplates,
     plugins: {
       import: pluginImport,
@@ -59,22 +23,6 @@ module.exports = [
       'unused-imports': pluginUnusedImports,
     },
     rules: {
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-      '@typescript-eslint/no-unused-expressions': 'off',
-      '@typescript-eslint/no-empty-function': 'off',
-      '@nx/enforce-module-boundaries': [
-        'error',
-        {
-          enforceBuildableLibDependency: true,
-          allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?js$'],
-          depConstraints: [
-            {
-              sourceTag: '*',
-              onlyDependOnLibsWithTags: ['*'],
-            },
-          ],
-        },
-      ],
       '@angular-eslint/directive-selector': [
         'error',
         {
@@ -108,9 +56,8 @@ module.exports = [
       ],
       'simple-import-sort/exports': 'warn',
       'unused-imports/no-unused-imports': 'warn',
-      'semi': 'off',
-      '@typescript-eslint/no-unused-vars': 'warn',
-      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-expressions': 'off',
+      '@typescript-eslint/no-empty-function': 'off',
     },
   },
   {
@@ -118,4 +65,4 @@ module.exports = [
     extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
     rules: {},
   },
-]
+)
