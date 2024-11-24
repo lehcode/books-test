a581b7363045e08f436fc843bbb3142e8db7def8
lehcode <53556648+lehcode@users.noreply.github.com>
2024-11-24 15:53:33 +0200

    refactor(config): Revert app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,12 @@
-import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http'
+import { provideHttpClient } from '@angular/common/http'
 import { ApplicationConfig, isDevMode } from '@angular/core'
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
 import { provideRouter, withComponentInputBinding, withRouterConfig } from '@angular/router'
 import { provideEffects } from '@ngrx/effects'
 import { provideStore } from '@ngrx/store'
 import { provideStoreDevtools } from '@ngrx/store-devtools'
+
 import { routes } from './app.routes'
-import { DEFAULT_TIMEOUT, TimeoutInterceptor } from './interceptors/timeout.interceptor'
 import { provideMaterialConfig } from './material.provider'
 import { BooksEffects } from './store/effects/books.effects'
 import { bookReducer } from './store/reducers/books.reducers'
@@ -14,8 +14,6 @@ import { bookReducer } from './store/reducers/books.reducers'
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
-    [{ provide: HTTP_INTERCEPTORS, useClass: TimeoutInterceptor, multi: true }],
-    [{ provide: DEFAULT_TIMEOUT, useValue: 90000 }],
     provideAnimationsAsync(),
     provideMaterialConfig(),
     provideStore({
