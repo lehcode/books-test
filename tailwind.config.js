80aad49b4fafdd560ceb740f9f8860133c0bfb73
lehcode <53556648+lehcode@users.noreply.github.com>
2024-11-26 09:25:35 +0200

    chore(styles): Update CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 import aspectRatio from "@tailwindcss/aspect-ratio";
 import typography from "@tailwindcss/typography";
-import colors from "tailwindcss/colors";
 
 const defaultTheme = require("tailwindcss/defaultTheme");
 
@@ -18,16 +17,36 @@ export default {
       "2xl": "1536px",
     },
     colors: {
-      gray: colors.gray,
-      blue: colors.sky,
-      red: colors.rose,
-      pink: colors.fuchsia,
-      purple: "#7e5bef",
-      orange: "#ff7849",
-      green: "#13ce66",
-      yellow: "#ffc82c",
-      "gray-dark": "#273444",
-      "gray-light": "#d3dce6",
+      gray: {
+        50: '#F9FAFB',
+        100: '#F3F4F6',
+        200: '#E5E7EB',
+        300: '#D1D5DB',
+        400: '#9CA3AF',
+        500: '#6B7280',
+        600: '#4B5563',
+        700: '#374151',
+        800: '#1F2937',
+        900: '#111827',
+      },
+      indigo: {
+        400: '#818cf8',
+        500: '#6366f1',
+        600: '#4f46e5',
+        900: '#312e81',
+      },
+      emerald: {
+        500: '#10b981',
+        600: '#059669',
+      },
+      rose: {
+        500: '#f43f5e',
+        600: '#e11d48',
+      }
     },
+    transitionProperty: {
+      'height': 'height',
+      'spacing': 'margin, padding',
+    }
   },
 };
